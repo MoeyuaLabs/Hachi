5b0a911998fa821f1108724c1af8228d22aed2d5
Moeyua <moeyua13@icloud.com>
2023-11-22 10:02:30 +0800

    🐎 ci: set github page

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,7 @@ import VueRouter from 'unplugin-vue-router/vite'
 import { VueRouterAutoImports } from 'unplugin-vue-router'
 
 export default defineConfig({
+  base: './hachi',
   resolve: {
     alias: {
       '~/': `${path.resolve(__dirname, 'src')}/`,
