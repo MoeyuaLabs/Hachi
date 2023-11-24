44da9c976fc33e82f02f2961bb1b9d5a6e8848f9
Moeyua <moeyua13@icloud.com>
2023-11-24 18:38:46 +0800

    🦄 refactor: ui

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-const { birthday, age, weight, history, requestHistory } = useHachi()
+const { weight, history, requestHistory } = useHachi()
 
 const chartRef = ref<HTMLElement | null>(null)
 
@@ -20,41 +20,49 @@ onMounted(async () => {
   await charInit(chartRef)
   requestHistory()
 })
+
+function formatData(data: number) {
+  return data.toFixed(2)
+}
+
+function calculateWeight(index: number) {
+  if (index <= 0)
+    return
+  const { weight: prevWeight } = history.value[index - 1]
+  const { weight } = history.value[index]
+  return formatData(weight - prevWeight)
+}
 </script>
 
 <template>
-  <TheHeader>
-    <template #left>
+  <el-container p-4>
+    <el-header height="40px" flex="~ items-center justify-between">
       <RouterLink to="/settings" i-carbon-settings icon-btn />
-    </template>
-    <template #right>
       <RouterLink to="/add" i-carbon-add icon-btn />
-    </template>
-  </TheHeader>
+    </el-header>
+    <el-main>
+      <div ref="chartRef" h-100 w-full bg-white pt-2 />
 
-  <div ref="chartRef" h-100 w-full bg-white pt-2 />
+      <div>
+        <div v-for="(h, i) in history" :key="h.id" mb-4 b-rd-2 bg-hex-eef1f7 p-4>
+          <div mb-4 text-left color-hex-3166f1 flex="~ items-center gap-2">
+            <div i-carbon-activity />
+            <span font-bold>Hachi</span>
+            <div i-carbon-dot-mark text-size-2 />
+            {{ h.date }}
+          </div>
 
-  <div flex="~ justify-around items-center" h-16 w-full>
-    <div>
-      <p>Birthday</p>
-      <p>{{ birthday.toLocaleDateString() }}</p>
-    </div>
-    <div h-full w-1 b-l="1px solid black" />
-    <div>
-      <p>Age</p>
-      <p>{{ age }} day</p>
-    </div>
-  </div>
-
-  <div p="x-4 y-6">
-    <h2 w-full pl-5 text-left>
-      History
-    </h2>
-    <div b-rd-5 bg-white px-5>
-      <div v-for="h in history" :key="h.id" w-full flex justify-between p-2 b-b="1px solid hex-DADADB">
-        <span>{{ h.weight }} g</span>
-        <span>{{ h.date }}</span>
+          <div flex>
+            <div mr-8>
+              <span text-8 font-bold>{{ formatData(h.weight) }}</span> <span>g</span>
+            </div>
+            <div v-if="i > 0">
+              <span i-carbon-caret-sort-up color="green" inline-block />
+              <span text-8 font-bold>{{ calculateWeight(i) }}</span> <span>g</span>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
+    </el-main>
+  </el-container>
 </template>
